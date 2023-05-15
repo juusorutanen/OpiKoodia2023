a782a97ac0283073ee5a5b1bebcfd1e3a8a4cd1d
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-05-15 11:41:03 +0300

    commit

---
+++ javascript/02_buttons_and_stuff/public/script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+function changeColor() {
+    let header = document.getElementById("header")
+    
+    let colorpicker = "ABCDEF0123456789"
+
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        let temp = Math.floor(Math.random() * 16);
+        color = color + colorpicker[temp]
+    }
+    header.style.color = color;
+
+    console.log(color)
+    
+}
