90a940de66693f548cfe4587153933aa3b19fb6d
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-06-12 14:29:27 +0300

    create components

---
+++ react/13_number_guess_game/src/components/GamePage.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import useGame from "../hooks/useGame";
+
+const GamePage = (props) => {
+    const [state,setState] = useState({
+        guess:0
+    })
+
+    const {message,guess} = useGame();
+
+    const onChange = (event) => {
+        setState({
+            guess:event.garget.value
+        })
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        guess(state.guess);
+    }
+
+    return (
+        <div style={{margin:"auto"}}>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="guess">Guess a number</label>
+                <input type="number"
+                        name="guess"
+                        id="guess"
+                        onChange={onChange}
+                        value={state.guess}
+                />
+                <br/>
+                <input type="submit" value="Guess"></input>
+            </form>
+            <h3>{message}</h3>
+        </div>
+    )
+}
+
+export default GamePage;
