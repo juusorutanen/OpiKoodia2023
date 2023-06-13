7b3eaf8b3499f6ba8391c6aeb981dae0b4d749e8
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-06-13 12:46:22 +0300

    commit statemanager

---
+++ react/50_shopping_app/04_stage_statemanager/frontend/src/components/Navbar.js
@@ -0,0 +1,46 @@
+import {Link} from 'react-router-dom';
+import useAppState from '../hooks/useAppState';
+import '../App.css';
+import useAction from '../hooks/useAction';
+
+const Navbar = (props) => {
+
+        const {isLogged,user} = useAppState();
+        const {logout} = useAction();
+
+    if(isLogged) {
+    return (
+        <nav style={{
+            backgroundColor:"#183f33"}}
+        className="navbar navbar-expand-lg navbar-light">
+            <p className="navbar-brand" style={{"marginLeft":10,color:"white"}}><Link to="/" className="nav-link">Shopping App</Link></p>
+            <ul className="navbar-nav">
+                <li className="nav-item" style={{"marginLeft":10}}>
+                    <Link to="/" className="nav-link" style={{"color":"white"}}>Shopping List</Link>
+                </li>
+                <li className="nav-item" style={{"marginLeft":10}}>
+                    <Link to="/form" className="nav-link" style={{"color":"white"}}>Add new item</Link>
+                </li>
+                <li className="nav-item" style={{"marginLeft":10}}>
+                    <Link to="/" className="nav-link" onClick={logout} style={{"color":"white"}}>Log out</Link>
+                </li>
+                <li className="nav-item" style={{"marginLeft":10}}>
+                    <p className="nav-link" style={{"color":"#32cd32"}}>Logged in as {user}</p>
+                </li>
+            </ul>
+        </nav>
+    )
+} else {
+    return (
+    <nav style={{
+        backgroundColor:"#183f33"}}
+        className="navbar navbar-expand-lg navbar-light">
+        <a className="navbar-brand" style={{"marginLeft":10,color:"white"}}>Shopping App</a>
+        <ul className="navbar-nav">
+        </ul>
+    </nav>
+    )
+}
+}
+
+export default Navbar;
