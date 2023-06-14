a5e487f3f1ddc873c212738935a040a8c41f5d06
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-06-14 11:38:42 +0300

    commit

--- react/60_typescript/03_lists_and_forms/src/App.tsx
+++ react/60_typescript/03_lists_and_forms/src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './models/Contact';
+import ContactForm from './components/ContactForm';
+import ContactList from './components/ContactList';
+
+interface State {
+  list:Contact[];
+  id:number;
+}
 
 function App() {
+
+  const [state,setState] = useState<State>({
+    list:[],
+    id:100
+  })
+
+  const addContact = (contact:Contact) => {
+    setState((state) => {
+      contact.id = state.id;
+      return {
+        list:state.list.concat(contact),
+        id:state.id+1
+      }
+    })
+  }
+
+  const removeContact = (id:number) => {
+    setState((state) => {
+      let tempList = state.list.filter(contact => contact.id !== id);
+      return {
+        ...state,
+        list:tempList
+      }
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactForm addContact={addContact}/>
+      <hr/>
+      <ContactList list={state.list} removeContact={removeContact} />
     </div>
   );
 }
