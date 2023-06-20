3a06a998ee87afa24c0bcabfc795ea2c6edc36d9
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-06-20 11:29:04 +0300

    commit exercise

--- react/60_typescript/07_shopping_app_with_redux/src/App.tsx
+++ react/60_typescript/07_shopping_app_with_redux/src/App.tsx
@@ -4,6 +4,9 @@ import LoginPage from './components/LoginPage';
 import {useSelector} from 'react-redux';
 import {AppState} from './types/states';
 import Navbar from './components/Navbar';
+import ShoppingForm from './components/ShoppingForm';
+import ShoppingList from './components/ShoppingList';
+import {Routes,Route,Navigate} from 'react-router-dom';
 
 function App() {
 
@@ -20,14 +23,31 @@ function App() {
   if(state.login.error) {
     messageArea = <h4 style={{height:40}}>{state.login.error}</h4>
   }
+  if(state.login.isLogged) {
+    return (
+      <div className="App">
+      <Navbar/>
+      {messageArea}
+      <Routes>
+        <Route path="/" element={<ShoppingList/>} />
+        <Route path="/form" element={<ShoppingForm/>} />
+        <Route path="*" element={<Navigate to ="/"/>} />
+      </Routes>
+      </div>
+    );
+  } else {
 
   return (
     <div className="App">
       <Navbar/>
       {messageArea}
-      <LoginPage/>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="*" element={<Navigate to ="/"/>} />
+      </Routes>
     </div>
   );
+  }
 }
 
 export default App;
